090f09ac8217afa68c4eeba7ddcba2da8da3d74c
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-09-25 19:34:12 +0330

    FIX: fixing inconsistent use of space and tabs error in costFunctions.py

--- src/costFunctions.py
+++ src/costFunctions.py
@@ -1,22 +1,29 @@
 import numpy as np 
 from utils import sigmoid_prime 
+
+
+class CostFunction(object) :
+	pass
                                                                                                                                                       
 class QuadraticCost(CostFunction) :                                                                                                                   
-    @staticmethod
-    def get_cost(a , y) :
-
-        return 0.5 * np.linalg.norm(a - y) ** 2                                                                                                                
-    @staticmethod
-    def get_delta(z , a , y) :
-
-        return (a - y) * sigmoid_prime(z)                                                                                                             
-class CrossEntropyCost(CostFunction) :                                                                                                                
-    @staticmethod
+	@staticmethod
 	def get_cost(a , y) :
+	
+		return 0.5 * np.linalg.norm(a - y) ** 2
 
-        return np.sum(np.nan_to_num(-y*np.log(a) - (1 - y)*np.log(1 - a))) 
-                                                                                                                                                      
-    @staticmethod
-    def get_delta(z , a , y) :  
+	@staticmethod
+	def get_delta(z , a , y) :
 
-        return (a - y) 
+		return (a - y) * sigmoid_prime(z)
+
+class CrossEntropyCost(CostFunction) :
+
+	@staticmethod
+	def get_cost(a , y) :
+	
+		return np.sum(np.nan_to_num(-y*np.log(a) - (1 - y)*np.log(1 - a))) 
+	                                                                                                                                                  
+	@staticmethod
+	def get_delta(z , a , y) :
+	
+		return (a - y) 
